202a34a593dbb8370fbe82c1d6d62b856f7b2c9f
Sonal919 <71544103+Sonal919@users.noreply.github.com>
2020-09-24 20:14:58 +0530

    Update remote-state.tf

--- aws/6_remote_state/remote-state.tf
+++ aws/6_remote_state/remote-state.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "techlanders-statefile"
-    key  = "terraform/state"
-    region = "us-east-2"
+    bucket  = "sonaljen"
+    key  = "Sonal"
+    region = "us-east-1"
 #   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
 #   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
   }
